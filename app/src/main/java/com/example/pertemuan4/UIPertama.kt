91d681600f42e72f2c4704a34afae496445458f2
Jaidil14 <jaidilrachmad@gmail.com>
2025-10-24 20:05:01 +0700

    Modifikasi kode di file UIPertama.kt dan file resource (strings.xml) untuk keperluan tugas praktikum

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -2,10 +2,12 @@ package com.example.pertemuan4
 
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -54,17 +56,95 @@ fun AktivitasPertama(modifier: Modifier) {
                 Spacer(modifier = Modifier.width(30.dp))
                 Column() {
                     Text(
-                        stringResource(R.string.nama),
+                        stringResource(R.string.nama1),
                         fontSize = 30.sp,
                         fontFamily = FontFamily.Cursive,
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
-                        stringResource(R.string.alamat),
+                        stringResource(R.string.alamat1),
                         fontSize = 20.sp,
                         color = Color.Yellow,
                         modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Blue
+            )) {
+            Row() {
+                val gambar = painterResource( R.drawable.logo_umj)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size( 100.dp).padding( 10.dp),
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        stringResource(R.string.nama2),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.hp2),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamat2),
+                        fontSize = 15.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                }
+            }
+        }
+        Spacer( modifier = Modifier.height(5.dp))
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Green
+            )) {
+            Row() {
+                val gambar = painterResource( R.drawable.logo_umm)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size( 100.dp).padding( 10.dp)
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        stringResource(R.string.nama3),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.hp3),
+                        fontSize = 20.sp,
+                        color = Color.Black,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+                    Text(
+                        stringResource(R.string.alamat3),
+                        fontSize = 15.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 5.dp)
                     )
                 }
             }
